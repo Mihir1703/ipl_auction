877d8a8e347c124a3e45c26f2f27dad49392e94a
Mihir1703 <mihirwaykole1703@yahoo.com>
2022-02-18 18:30:12 +0530

    singin active

--- esummit22/user_info/urls.py
+++ esummit22/user_info/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_user),
     path('logout/', views.logout_user),
+    path('signup/', views.register_user),
     path('', views.index),
     path('player/<int:id>/', views.single),
     path('player/<str:tag>/', views.skill),
